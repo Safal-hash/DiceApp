d55bfa1545638893104e68e578e9d6ab12f6d77c
Safal Tiwari <Safaltiwari2@gmail.com>
2023-02-19 22:01:43 +0545

    imported the assests to the main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(),
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Center(
+            child: Text("Dice App"),
+          ),
+          backgroundColor: Colors.redAccent,
+        ),
+        body: const DicePage(),
+      ),
+    ),
   );
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded(
+          child: Image.asset('images/dice2.png'),
+        ),
+      ],
+    );
+  }
 }
